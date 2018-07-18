6207f0f8908f6b590cc057f78aef406372f1f7dd
nguyenloc96 <nguyenloc96fit@gmail.com>
2018-07-18 18:05:02 +0700

    fix menu

--- js/layout.js
+++ js/layout.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+    $('.menu-title').hover(function(){
+        var curMenu = parseInt($(this).attr('per-menu'));
+        console.log("hover",curMenu);
+        $('.dropdown-menu per-menu').eq(curMenu).addClass('show');
+    });
+    
     $('#menu .menu-item').on('click', function(){
         $('#menu .menu-item').removeClass('selected');
         $(this).addClass('selected');
@@ -6,5 +12,11 @@ $(document).ready(function(){
     $('.page-register').click(function(){
         $('#login').addClass('d-none');
         $('#register').removeClass('d-none');
+    });
+    $('#sign-in').click(function(){
+        $('#sign-in').disable = true;
+        $('#sign-out').removeClass('d-none');
+        $('#login').removeClass('d-none');
+        $('#register').addClass('d-none');
     });
 });
