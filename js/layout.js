069c8157bf6e0273421cb9bf3e317eb387d90daf
nguyenloc96 <nguyenloc96fit@gmail.com>
2018-07-19 11:59:08 +0700

    fix hover menu dropdown

--- js/layout.js
+++ js/layout.js
@@ -1,20 +1,26 @@
-$(document).ready(function(){
-    $('.menu-title').hover(function(){
-        var curMenu = parseInt($(this).attr('per-menu'));
-        console.log("hover",curMenu);
-        $('.dropdown-menu per-menu').eq(curMenu).addClass('show');
+$(document).ready(function () {
+    $('.menu-item span').mouseover(function () {
+        $('.dropdown-menu').removeClass('show');
+        var perMenu = parseInt($(this).attr('menu'));
+        console.log(perMenu);
+        $('.dropdown-menu').eq(perMenu).addClass('show');
     });
-    
-    $('#menu .menu-item').on('click', function(){
+    $('.dropdown-menu').mouseleave(function(){
+        setTimeout(() => {
+            $(this).removeClass('show');
+        }, 1000);
+        
+    });
+    $('#menu .menu-item').on('click', function () {
         $('#menu .menu-item').removeClass('selected');
         $(this).addClass('selected');
     });
-    $('.page-register').click(function(){
+    $('.page-register').click(function () {
         $('#login').addClass('d-none');
         $('#register').removeClass('d-none');
     });
-    $('#sign-in').click(function(){
-        $('#sign-in').disable = true;
+    $('#sign-in').click(function () {
+        $('#sign-in').attr('disabled', true);
         $('#sign-out').removeClass('d-none');
         $('#login').removeClass('d-none');
         $('#register').addClass('d-none');
